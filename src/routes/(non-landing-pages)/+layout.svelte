cefc160ac76b769b2b68e96613ca66ca22422277
Matthew Mong <matthew.mong1999@gmail.com>
2024-04-04 01:01:53 -0400

    fix a missing tag

--- src/routes/(non-landing-pages)/+layout.svelte
+++ src/routes/(non-landing-pages)/+layout.svelte
@@ -1,3 +1,5 @@
 <div class="hiText">
     <h1>I'm Matthew</h1>
 </div>
+
+<slot></slot>
--- src/routes/(non-landing-pages)/about/+page.svelte
+++ src/routes/(non-landing-pages)/about/+page.svelte
@@ -1,2 +1,2 @@
-<h2>I make stuff</h2>
-<p>This is a paragraph.</p>
+<h2>Usually it's software, but sometimes it's music</h2>
+<p>This is a really cool paragraph about what I do.</p>
