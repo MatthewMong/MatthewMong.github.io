ae351c0b1e04172ffa1ac3d33fd1ab08bb606db3
Matthew Mong <matthew.mong1999@gmail.com>
2024-04-03 20:30:34 -0400

    use github pages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,1 @@
+<h1>GET OUTTA HERE</h1>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+</script>
+
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/settings">Settings</a>
+</nav>
+
+<slot></slot>
